cb23953d84ae13d5082f665b1de3711b3088c895
Valdio Veliu <valdioveliu@gmail.com>
2020-09-20 12:03:16 +0200

    add task description

--- maze.py
+++ maze.py
@@ -1,8 +1,16 @@
 import math
 import numpy as np
 
+
+"""
+Task: Try to find the route in the provided maze from origin (0,0) to destination (N-1,M-1).
+N-number of rows, M-number of columns.
+
+The maze is represented as a matrix of bits, where 0 represents an empty slot and 1 represents a wall.
 # 0 -> empty
 # 1 -> wall
+Find the connected coordinates with value of 0 that connect from start to destination.
+"""
 maze = [
     [0, 0, 1, 1, 0, 0, 1, 1],
     [1, 0, 1, 0, 0, 1, 1, 0],
