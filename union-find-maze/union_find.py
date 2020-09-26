87cd9e93d195d6c353db0a1183f5b9c027d32534
Valdio Veliu <valdioveliu@gmail.com>
2020-09-26 22:31:59 +0200

    cleanup

--- union-find-maze/union_find.py
+++ union-find-maze/union_find.py
@@ -26,20 +26,3 @@ def union(data, i, j):
 
 def connected(data, i, j):
     return find(data, i) == find(data, j)
-
-
-# ------------------------------------------------------------
-# Test data
-# ------------------------------------------------------------
-# n = 10
-# data = [i for i in range(n)]
-
-# # connections = [(0, 1), (1, 2), (0, 9), (5, 6), (6, 4), (5, 9)]
-# connections = [(6, 1), (5, 6)]
-# # union
-# for i, j in connections:
-#     union(data, i, j)
-
-# # find
-# for i in range(n):
-#     print('item', i, '-> component', find_immediate(data, i))
