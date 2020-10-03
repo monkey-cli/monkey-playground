cc8bf63fee506264eda8bcc9519d9a8252e1ce86
Valdio Veliu <valdioveliu@gmail.com>
2020-10-04 00:00:56 +0200

    fix imports

--- union_find_maze/visualizer/maze_visualizer.py
+++ union_find_maze/visualizer/maze_visualizer.py
@@ -1,39 +1,10 @@
 import numpy as np
 from turtle import Screen, Turtle
-import os
-import sys
-cwd = os.getcwd()
-
-# ------------------------------------------------------------------------------------
+from visualizer import utils
+import mock
 
 
-def import_module_by_path(path):
-    name = os.path.splitext(os.path.basename(path))[0]
-    if sys.version_info[0] == 2:
-        # Python 2
-        import imp
-        return imp.load_source(name, path)
-    elif sys.version_info[:2] <= (3, 4):
-        # Python 3, version <= 3.4
-        from importlib.machinery import SourceFileLoader
-        return SourceFileLoader(name, path).load_module()
-    else:
-        # Python 3, after 3.4
-        import importlib.util
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        return mod
-
-
-# ------------------------------------------------------------------------------------
-# Imports
-mock = import_module_by_path(cwd + '/union_find_maze/mock.py')
-
-utils = import_module_by_path(
-    cwd + '/union_find_maze/visualizer/utils.py')
 draw_graph, fill_matrix, fill_coordinate, is_matrix_edge = utils.draw_graph, utils.fill_matrix, utils.fill_coordinate, utils.is_matrix_edge
-
 
 # ------------------------------------------------------------------------------------
 
